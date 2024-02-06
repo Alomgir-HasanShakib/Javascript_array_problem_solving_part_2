ba0597efd6b1c1c19356d64a03f5f6df35e88ff6
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2024-02-06 17:25:30 +0600

    solved task 2 problems

--- task2.js
+++ task2.js
@@ -2,3 +2,13 @@
 // Find the friend with the smallest name.
 // const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
 
+const heights2 = ["rahim", "robin", "rafi", "ron", "rashed"];
+let smallName = heights2[0];
+
+for (const name of heights2) {
+  //   console.log(name);
+  if (name.length < smallName.length) {
+    smallName = name;
+  }
+}
+console.log(smallName);
